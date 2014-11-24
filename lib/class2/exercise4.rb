c687fbd32370f8806ed7b5997faad26dba42acd0
Charles Crawford III <charliec364@gmail.com>
2014-11-24 07:13:16 +0000

    Finish class 2

--- lib/class2/exercise4.rb
+++ lib/class2/exercise4.rb
@@ -13,3 +13,22 @@
 #   What's your last name?
 #   Jackson
 #   Nice to meet you, Samuel Leroy Jackson.
+
+# Probably taking DRY to it's logical extreme...
+# But having some fun wiith it! :)
+name = ['first', 'middle', 'last'].map do |e|
+  puts "What's your #{e} name?"
+  gets.chomp
+end
+puts "Nice to meet you, #{name.join(' ')}."
+
+# This would be my student version though
+=begin
+puts "What's your first name?"
+first_name = gets.chomp
+puts "What's your middle name?"
+middle_name = gets.chomp
+puts "What's your last name?"
+last_name = gets.chomp
+puts "Nice to meet you, #{first_name} #{middle_name} #{last_name}."
+=end
